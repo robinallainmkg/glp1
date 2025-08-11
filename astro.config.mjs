16f002e10df53676e2ca87436a31c6926adf1bff
Robin GLP1 <robin@glp1france.com>
2025-08-11 08:15:47 +0200

    Deploy: Mise a jour automatique

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: 'https://glp1-france.fr',
   base: '/',
   output: 'static',
-  integrations: [sitemap()],
+  integrations: [sitemap(), tailwind()],
   server: {
     port: 4321,
     open: true
