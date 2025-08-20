8c89d4df07ccad791e7a89e50b52b7fdc22844bc
Robin Allain <robin@glp1-france.fr>
2025-08-20 17:53:42 +0200

    Deploy: Production avec TinaCMS et documentation

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ cacheDir: 'C:/Users/robin/Documents/glp1official/glp1new/glp1-github/tina/__generated__/.cache/1755704854687', url: 'https://content.tinajs.io/1.6/content/d2c40213-494b-4005-94ad-b601dbdf1f0e/github/production', token: 'f5ae6be5c8b2d3dbb575918e775be87d1f4ec29d', queries,  });
+export const client = createClient({ cacheDir: 'C:/Users/robin/Documents/glp1official/glp1new/glp1-github/tina/__generated__/.cache/1755705168989', url: 'https://content.tinajs.io/1.6/content/d2c40213-494b-4005-94ad-b601dbdf1f0e/github/production', token: 'f5ae6be5c8b2d3dbb575918e775be87d1f4ec29d', queries,  });
 export default client;
   
